d731c69850acdb81cffc88f9dc75a9c15b4dd995
Cristi Buzzi <buzzi_cristi@yahoo.com>
2023-05-14 13:06:26 +0300

    Creation of analyses and database diagram

---
+++ src/main/java/com/sda/onlineshop/SolarPanelsShop/SolarPanelsShopApplication.java
@@ -0,0 +1,13 @@
+package com.sda.onlineshop.SolarPanelsShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarPanelsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarPanelsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/onlineshop/SolarPanelsShop/SolarPanelsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.onlineshop.SolarPanelsShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarPanelsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

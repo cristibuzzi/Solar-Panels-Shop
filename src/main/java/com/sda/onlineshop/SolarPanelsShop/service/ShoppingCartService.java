f0826d0778cd88d8d642692a2d901ce9943a54a3
Cristi Buzzi <buzzi_cristi@yahoo.com>
2023-05-25 10:41:13 +0300

    SPS-13: Implement add to cart

---
+++ src/main/java/com/sda/onlineshop/SolarPanelsShop/service/ShoppingCartService.java
@@ -0,0 +1,19 @@
+package com.sda.onlineshop.solarpanelsshop.service;
+
+import com.sda.onlineshop.solarpanelsshop.model.Product;
+
+import java.util.Map;
+
+public interface ShoppingCartService {
+    void addProduct(Product product);
+
+    void removeProduct(Product product);
+
+    void clearProducts();
+
+    double totalPrice();
+
+    void checkOut();
+
+    Map<Product, Integer> getAllProducts();
+}
